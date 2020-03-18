618a98b0602b14b56d4d5e6ef6a41371831f5197
Solitasroh <solitasroh@gmail.com>
2020-03-19 00:06:57 +0900

    css 를 위한 class 작업

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
 import Movie from "./Movie"
+import "./App.css"
 // https://yts-proxy.now.sh/list_movies.json
 class App extends React.Component {
     state = {
@@ -21,24 +22,30 @@ class App extends React.Component {
     }
 
     render() {
-      const {
-        isLoading, movies
-      } = this.state;
-
-      return ( <div> {
-          isLoading ? "Is loading..." : 
-          movies.map(movie => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-              />
-            ))
-        } </div>);
-      }
+      const { isLoading, movies } = this.state;
+      return (
+        <section class="container">
+          {isLoading ? (
+            <div class="loader">
+              <span class="loader__text">Loading...</span>
+            </div>
+          ) : (
+            <div class="movies">
+              {movies.map(movie => (
+                <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary}
+                  poster={movie.medium_cover_image}
+                />
+              ))}
+            </div>
+          )}
+        </section>
+      );
     }
+  }
 
-    export default App;
+export default App;
