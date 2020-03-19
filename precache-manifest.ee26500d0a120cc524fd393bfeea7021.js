09e67277e117261264f32dc694122fa2f0f7d6bf
solitas <solitas.roh@kakao.com>
2020-03-19 21:01:47 +0900

    Updates

---
+++ precache-manifest.ee26500d0a120cc524fd393bfeea7021.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "277b1df9ee23dcb35fce818b01e8c0f8",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "2b3da8d051c899430e92",
+    "url": "/react-movie-app/static/css/main.a92bfb2d.chunk.css"
+  },
+  {
+    "revision": "fbe3dfbe7aea9dee0295",
+    "url": "/react-movie-app/static/js/2.84a7d3ec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-movie-app/static/js/2.84a7d3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3da8d051c899430e92",
+    "url": "/react-movie-app/static/js/main.ebc00668.chunk.js"
+  },
+  {
+    "revision": "ee30f8a00c3886408ea0",
+    "url": "/react-movie-app/static/js/runtime-main.23d872c2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_movie_app/precache-manifest.b99088a3c0252d787c5050e22021a1a4.js"
+  "/react-movie-app/precache-manifest.ee26500d0a120cc524fd393bfeea7021.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_movie_app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-movie-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
