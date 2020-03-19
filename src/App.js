3fabee00f875724daef5f9b786ec31ac415d1a1b
solitas <solitas.roh@kakao.com>
2020-03-19 19:37:31 +0900

    1. genres 추가 2. 설정 변경

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
 import React from "react";
 import PropTypes from "prop-types";
 import axios from "axios";
-import Movie from "./Movie"
-import "./App.css"
+import Movie from "./Movie";
+import "./App.css";
 // https://yts-proxy.now.sh/list_movies.json
 class App extends React.Component {
-    state = {
-      isLoading: true,
-      movies: []
-    };
-    getMovies = async () => {
-      const { data : {
-          data: {movies}
-        }
-      } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-      this.setState({movies: movies, isLoading: false})
-    }
-    
-    componentDidMount() {
-      this.getMovies();
-    }
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.setState({ movies: movies, isLoading: false });
+  };
 
-    render() {
-      const { isLoading, movies } = this.state;
-      return (
-        <section class="container">
-          {isLoading ? (
-            <div class="loader">
-              <span class="loader__text">Loading...</span>
-            </div>
-          ) : (
-            <div class="movies">
-              {movies.map(movie => (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                />
-              ))}
-            </div>
-          )}
-        </section>
-      );
-    }
+  componentDidMount() {
+    this.getMovies();
   }
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section class="container">
+        {isLoading ? (
+          <div class="loader">
+            <span class="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div class="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
 
 export default App;
