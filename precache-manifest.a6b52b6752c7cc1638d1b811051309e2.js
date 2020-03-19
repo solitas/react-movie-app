a4b76f7eb3407b4fd6e713eb65354f55dbbf9058
solitas <solitas.roh@kakao.com>
2020-03-19 20:55:54 +0900

    Updates

---
+++ precache-manifest.a6b52b6752c7cc1638d1b811051309e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adfa8fca73c74797a50ce660f3386495",
+    "url": "/react-move-app/index.html"
+  },
+  {
+    "revision": "2b3da8d051c899430e92",
+    "url": "/react-move-app/static/css/main.a92bfb2d.chunk.css"
+  },
+  {
+    "revision": "fbe3dfbe7aea9dee0295",
+    "url": "/react-move-app/static/js/2.84a7d3ec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-move-app/static/js/2.84a7d3ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3da8d051c899430e92",
+    "url": "/react-move-app/static/js/main.ebc00668.chunk.js"
+  },
+  {
+    "revision": "e8703e8f088a79f4ff86",
+    "url": "/react-move-app/static/js/runtime-main.3913ac85.js"
+  }
+]);
